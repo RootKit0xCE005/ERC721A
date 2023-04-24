4efa1bf5ff27e43dd83edbb57fac94fddd7c489d
Rootkit0xCE005 <racingcars241@gmail.com>
2023-04-24 12:23:16 +0100

    commit

--- test/erc721a-test.js
+++ test/erc721a-test.js
@@ -4,7 +4,7 @@ const { parseEther, formatEther } = require("ethers/lib/utils");
 const { setBalance } = require("@nomicfoundation/hardhat-network-helpers");
 
 describe("[ ERC721A Test ]\n", () => {
-  let deployer, thanos, joker;
+  let deployerWallet, thanosWallet, jokerWallet;
   let deployerAddress, thanosAddress, jokerAddress;
   let cost1, cost2;
   let ipfsUri = "ipfs://your_link_to_ipfs_sotrage/";
@@ -18,9 +18,9 @@ describe("[ ERC721A Test ]\n", () => {
     contract.deployed();
 
     // setup the callers
-    deployer = contract.connect(user0);
-    thanos = contract.connect(user1);
-    joker = contract.connect(user2);
+    deployerWallet = contract.connect(user0);
+    thanosWallet = contract.connect(user1);
+    jokerWallet = contract.connect(user2);
 
     deployerAddress = user0.address;
     thanosAddress = user1.address;
@@ -35,13 +35,13 @@ describe("[ ERC721A Test ]\n", () => {
 
   describe("Mint Function", () => {
     beforeEach(async () => {
-      await thanos.mint(5, { value: cost1 });
-      await joker.mint(15, { value: cost2 });
+      await thanosWallet.mint(5, { value: cost1 });
+      await jokerWallet.mint(15, { value: cost2 });
     });
 
     it("should mint nfts", async () => {
-      expect(await thanos.balanceOf(thanosAddress)).to.eq(5);
-      expect(await joker.balanceOf(jokerAddress)).to.eq(15);
+      expect(await thanosWallet.balanceOf(thanosAddress)).to.eq(5);
+      expect(await jokerWallet.balanceOf(jokerAddress)).to.eq(15);
 
       expect(await ethers.provider.getBalance(contract.address)).to.eq(
         // cost1 + cost2
@@ -51,27 +51,27 @@ describe("[ ERC721A Test ]\n", () => {
 
     it("should revert if didn't send enough ether ", async () => {
       // try to mint 10 nfts with 0.5
-      await expect(thanos.mint(10, { value: cost1 })).to.reverted;
+      await expect(thanosWallet.mint(10, { value: cost1 })).to.reverted;
     });
 
     it("should buyers owns the nfts", async () => {
       for (let i = 0; i < 5; i++)
-        expect(await thanos.ownerOf(i)).to.eq(thanosAddress);
+        expect(await thanosWallet.ownerOf(i)).to.eq(thanosAddress);
 
       for (let i = 5; i < 20; i++)
-        expect(await joker.ownerOf(i)).to.eq(jokerAddress);
+        expect(await jokerWallet.ownerOf(i)).to.eq(jokerAddress);
     });
 
     it("Revert if contract paused", async () => {
       await contract.pause(true);
-      await expect(deployer.mint(5, { value: cost1 })).to.reverted;
+      await expect(deployerWallet.mint(5, { value: cost1 })).to.reverted;
     });
 
     it("Revert if max nft limit exceeded", async () => {
       // we minted 20 nfts + 9980 == 10000
-      await deployer.mint(9980);
+      await deployerWallet.mint(9980);
       // if 10000 is minted we cant mint any more
-      await expect(deployer.mint(1)).to.reverted;
+      await expect(deployerWallet.mint(1)).to.reverted;
     });
 
     it("return eth if user sent more than it required", async () => {
@@ -86,41 +86,41 @@ describe("[ ERC721A Test ]\n", () => {
 
   describe("Other functiosn", () => {
     it("Get tokenUri", async () => {
-      await thanos.mint(5, { value: cost1 });
-      expect(await deployer.tokenURI(1)).to.eq(ipfsUri + "1.json");
+      await thanosWallet.mint(5, { value: cost1 });
+      expect(await deployerWallet.tokenURI(1)).to.eq(ipfsUri + "1.json");
     });
 
     it("total minted should equal 10000", async () => {
-      await deployer.mint(10000);
-      expect(await deployer.totalMinted()).to.eq(10000);
+      await deployerWallet.mint(10000);
+      expect(await deployerWallet.totalMinted()).to.eq(10000);
     });
   });
 
   describe("OnlyOwner functions", () => {
     it("should transfer Ownership", async () => {
-      await deployer.transferOwnership(thanosAddress);
-      expect(await deployer.owner()).to.eq(thanosAddress);
+      await deployerWallet.transferOwnership(thanosAddress);
+      expect(await deployerWallet.owner()).to.eq(thanosAddress);
     });
 
     it("should change baseURI", async () => {
       newBaseURI = "ipfs://123456789Abcdefj/";
 
-      await deployer.setBaseURI(newBaseURI);
-      expect(await deployer.baseURI()).to.eq(newBaseURI);
+      await deployerWallet.setBaseURI(newBaseURI);
+      expect(await deployerWallet.baseURI()).to.eq(newBaseURI);
     });
 
     it("nft cost should be updated", async () => {
       newPrice = parseEther("1");
 
-      await deployer.setCost(newPrice);
+      await deployerWallet.setCost(newPrice);
       expect(await contract.cost()).to.eq(newPrice);
     });
 
     it("only Owner should be able to withdraw", async () => {
-      await expect(thanos.withdraw(thanosAddress)).to.reverted;
-      await expect(deployer.withdraw(deployerAddress)).not.to.reverted;
+      await expect(thanosWallet.withdraw(thanosAddress)).to.reverted;
+      await expect(deployerWallet.withdraw(deployerAddress)).not.to.reverted;
 
-      // send contract balance to deployer address
+      // send contract balance to deployerWallet address
       expect(await ethers.provider.getBalance(contract.address)).to.eq(0);
     });
   });
